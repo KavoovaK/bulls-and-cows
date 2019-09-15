679e8b06c6470fc7d562664c1281ffd8598250d6
KavoovaK <pizzadebila@mail.ru>
2019-09-15 16:16:19 +0700

    Add function menu, enemy_comp, player

---
+++ Bulls_and_cows.h
@@ -0,0 +1,16 @@
+#ifndef BULLS_AND_COWS_BULLS_AND_COWS_H
+#define BULLS_AND_COWS_BULLS_AND_COWS_H
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+
+using namespace std;
+
+void menu();
+void info();
+void enemy_comp();
+void player();
+
+#endif //BULLS_AND_COWS_BULLS_AND_COWS_H
